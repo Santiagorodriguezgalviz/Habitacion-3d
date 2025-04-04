9801e99a74d9773f825fd3132485c34d1528a073
Santiagorodriguezgalviz <rodriguezsanti95@gmail.com>
2025-04-04 15:28:58 -0500

    resolviendo errores de dependencias

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -1,13 +1,17 @@
 const { merge } = require('webpack-merge')
 const commonConfiguration = require('./webpack.common.js')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(
     commonConfiguration,
     {
         mode: 'production',
-        plugins:
-        [
+        optimization: {
+            minimize: true,
+            minimizer: [new TerserPlugin()]
+        },
+        plugins: [
             new CleanWebpackPlugin()
         ]
     }
